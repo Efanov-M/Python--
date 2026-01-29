84aeeac2d3b0d2e506e964578da2131c255d65a0
Efanov-M <m.efanov@bk.ru>
2026-01-29 05:30:11 +0300

    Редактирование докстрима в файле

--- Open_read_file/Open_read_file_task1/task1_2.py
+++ Open_read_file/Open_read_file_task1/task1_2.py
@@ -7,13 +7,42 @@
 
 
 class Ingredient:
+    """
+    Класс Ingredient описывает один ингредиент рецепта.
+
+    Хранит:
+    - название ингредиента
+    - количество
+    - единицу измерения
+
+    Используется как элемент рецепта.
+    """
 
     def __init__(self, name, quantity, measure):
+        """
+        Создаёт объект ингредиента.
+
+        :param name: Название ингредиента (строка)
+        :param quantity: Количество ингредиента (целое число)
+        :param measure: Единица измерения (строка)
+        """
         self.name = name
         self.quantity = quantity
         self.measure = measure
 
     def to_dict(self):
+        """
+        Преобразует объект ингредиента в словарь.
+
+        Используется для формирования итоговой структуры cook_book.
+
+        :return: dict вида:
+                 {
+                     "ingredient_name": <название>,
+                     "quantity": <количество>,
+                     "measure": <единица измерения>
+                 }
+        """
         return {
             "ingredient_name": self.name,
             "quantity": self.quantity,
@@ -22,15 +51,39 @@ class Ingredient:
 
 
 class Recipe:
+    """
+    Класс Recipe описывает один рецепт блюда.
+
+    Содержит:
+    - название блюда
+    - список ингредиентов (объекты Ingredient)
+    """
 
     def __init__(self, name,):
+        """
+         Создаёт объект рецепта.
+
+        :param name: Название блюда (строка)
+        """
         self.name = name
         self.ingredients = []
 
     def add_ingredient(self, ingredient):
+        """
+        Добавляет ингредиент в рецепт.
+
+        :param ingredient: Объект класса Ingredient
+        """
         self.ingredients.append(ingredient)
 
     def to_list(self):
+        """
+        Преобразует рецепт в список словарей ингредиентов.
+
+        Каждый ингредиент конвертируется через ingredient.to_dict().
+
+        :return: список словарей ингредиентов
+        """
         result = []
         for ingredient in self.ingredients:
             result.append(ingredient.to_dict())
@@ -39,14 +92,39 @@ class Recipe:
 
 
 class CookBook:
+    """
+    Класс CookBook представляет сборник рецептов.
+
+    Хранит рецепты в виде словаря:
+    {
+        "Название блюда": объект Recipe
+    }
+    """
 
     def __init__(self):
+        """
+        Создаёт пустую кулинарную книгу.
+        """
         self.recipes = {}
 
     def add_recipe(self, recipe):
+        """
+        Добавляет рецепт в кулинарную книгу.
+
+        :param recipe: Объект класса Recipe
+        """
         self.recipes[recipe.name] = recipe
 
     def to_dict(self):
+        """
+        Преобразует всю кулинарную книгу в словарь формата cook_book.
+
+        :return: dict вида:
+                 {
+                     "Название блюда": [список ингредиентов],
+                     ...
+                 }
+        """
         result = {}
         for name, recipe in self.recipes.items():
             result[name] = recipe.to_list()
@@ -85,6 +163,27 @@ while i < len(lines):
 
 
 def get_shop_list_by_dishes(book, dishes, person_count):
+    """
+    Формирует список покупок на основе выбранных блюд и количества персон.
+
+    Для каждого блюда:
+    - берёт ингредиенты
+    - умножает количество на person_count
+    - суммирует одинаковые ингредиенты
+
+    :param book: Объект CookBook
+    :param dishes: Список названий блюд (list[str])
+    :param person_count: Количество человек (int)
+
+    :return: dict вида:
+             {
+                 "Название ингредиента": {
+                     "measure": <единица измерения>,
+                     "quantity": <общее количество>
+                 },
+                 ...
+             }
+    """
     shop_list = {}
 
     for dish_name in dishes:
--- Open_read_file/Open_read_file_task2/task2.py
+++ Open_read_file/Open_read_file_task2/task2.py
@@ -1,33 +1,92 @@
 class TextFile:
+    """
+    Класс TextFile предназначен для работы с текстовым файлом.
+
+    Хранит:
+    - имя файла
+    - список строк, считанных из файла
+    """
 
     def __init__ (self,filename):
+        """
+        Создаёт объект текстового файла.
+
+        :param filename: Имя файла (строка)
+        """
         self.filename = filename
         self.lines = []
-    # читаем файл - открываем, считаем строки, добавляем в список
+
     def read(self):
+        """
+        Считывает файл построчно и сохраняет строки в список self.lines.
+
+        - файл открывается в кодировке UTF-8
+        - каждая строка очищается от символов переноса строки и пробелов
+        """
         with open(self.filename, "r", encoding="utf-8") as f:
             for line in f:
                 line= line.strip()
                 self.lines.append(line)
-    # считаем сколько строк в спске
+
     def line_count(self):
+        """
+        Возвращает количество строк в файле.
+
+        :return: количество строк (int)
+        """
         return len(self.lines)
-    # возвращаем заполненный списов
+
     def get_lines(self):
+        """
+        Возвращает список строк файла.
+
+        :return: список строк (list[str])
+        """
         return self.lines
 
 class FileMerger:
+    """
+    Класс FileMerger объединяет несколько текстовых файлов в один.
+
+    Файлы:
+    - добавляются как объекты TextFile
+    - сортируются по количеству строк
+    - записываются в итоговый файл
+    """
 
     def __init__(self):
+        """
+        Создаёт объект объединителя файлов.
+        """
         self.files = []
-    # добавляем файлы в список
+
     def add_file(self, text_file):
+        """
+        Добавляет текстовый файл в список файлов для объединения.
+
+        :param text_file: Объект класса TextFile
+        """
         self.files.append(text_file)
-    # сортируем их по длине
+
     def sort_by_line_count(self):
+        """
+        Сортирует файлы по количеству строк (по возрастанию).
+
+        Использует метод line_count() каждого файла.
+        """
         self.files.sort(key=lambda f: f.line_count())
-    # записываем в общий файл
+
     def write_to_file(self, output_filename):
+        """
+        Записывает объединённые данные в итоговый файл.
+
+        Формат записи:
+        - имя файла
+        - количество строк
+        - содержимое файла построчно
+
+        :param output_filename: Имя итогового файла (строка)
+        """
         with open(output_filename, "w", encoding="utf-8") as f:
             for file in self.files:
                 f.write(file.filename + "\n")
