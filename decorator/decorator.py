522a2b315b976b5d0bc6ccb8a70bfaed71f844e8
Efanov-M <m.efanov@bk.ru>
2026-04-23 13:00:46 +0400

    Update decorator.py

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -8,13 +8,13 @@ def logger(old_function):
     def new_function(*args, **kwargs):
         result = old_function(*args, **kwargs)
 
-        with open('main.log', 'a', encoding='utf-8') as log_file:
+        with open("main.log", "a", encoding="utf-8") as log_file:
             log_file.write(
                 f"{datetime.now()} | "
-                f"function: {old_function.__name__} | "
-                f"args: {args} | "
-                f"kwargs: {kwargs} | "
-                f"result: {result}\n"
+                f"{old_function.__name__} | "
+                f"args={args} | "
+                f"kwargs={kwargs} | "
+                f"result={result}\n"
             )
 
         return result
@@ -23,13 +23,13 @@ def logger(old_function):
 
 
 def test_1():
-    path = 'main.log'
+    path = "main.log"
     if os.path.exists(path):
         os.remove(path)
 
     @logger
     def hello_world():
-        return 'Hello World'
+        return "Hello World"
 
     @logger
     def summator(a, b=0):
@@ -39,25 +39,25 @@ def test_1():
     def div(a, b):
         return a / b
 
-    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+    assert "Hello World" == hello_world(), "Функция возвращает 'Hello World'"
     result = summator(2, 2)
-    assert isinstance(result, int), 'Должно вернуться целое число'
-    assert result == 4, '2 + 2 = 4'
+    assert isinstance(result, int), "Должно вернуться целое число"
+    assert result == 4, "2 + 2 = 4"
     result = div(6, 2)
-    assert result == 3, '6 / 2 = 3'
+    assert result == 3, "6 / 2 = 3"
 
-    assert os.path.exists(path), 'файл main.log должен существовать'
+    assert os.path.exists(path), "файл main.log должен существовать"
 
     summator(4.3, b=2.2)
     summator(a=0, b=0)
 
-    with open(path, encoding='utf-8') as log_file:
+    with open(path, encoding="utf-8") as log_file:
         log_file_content = log_file.read()
 
-    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    assert "summator" in log_file_content, "должно записаться имя функции"
     for item in (4.3, 2.2, 6.5):
-        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+        assert str(item) in log_file_content, f"{item} должен быть записан в файл"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_1()
